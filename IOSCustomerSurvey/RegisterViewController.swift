4b59487f0aee9be5ccc92168364420dd98ca8796
CarySong <cary.song@volvo.com>
2017-09-12 15:25:51 +0800

    check

--- IOSCustomerSurvey/RegisterViewController.swift
+++ IOSCustomerSurvey/RegisterViewController.swift
@@ -7,29 +7,30 @@
 //
 
 import UIKit
+import Alamofire
 
 class RegisterViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let parameters: Parameters = [
+            "DealerId": "633100",
+            "Name": "DealerName",
+            "ContactNumber": "1522208",
+            "Email": "1@3.com",
+            "Password":"123456",
+            "Address": "address"
+        ]
+        AFWrapper.dealerRegistration(Registration_URL,params: parameters,success: {(model) -> Void in
+            print(model)
+            
+        }) {
+            (error) -> Void in
+            print(error)
+        }
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+   
 }
